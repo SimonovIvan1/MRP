5b4cedd0ec0008dfda0a6e31f5402d4f9bc7c5f3
Simonov Ivan <ivan.happy@mail.ru>
2024-03-17 13:49:46 +0300

    added new controllers

--- MRP_Admin_Api/Controllers/GoodsController.cs
+++ MRP_Admin_Api/Controllers/GoodsController.cs
@@ -1,12 +1,68 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExternalModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MRP_DAL.Repository;
+using MRP_DAL;
 
 namespace MRP_Admin_Api.Controllers
 {
+    [ApiController]
+
+    [Route("api/admin/goods")]
     public class GoodsController : Controller
     {
-        public IActionResult Index()
+        private readonly GoodsRepository _repository;
+
+        public GoodsController(DbContextOptions<AppDbContext> db)
         {
-            return View();
+            _repository = new GoodsRepository(db);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var clients = await _repository.GetAll();
+                if (clients == null) return NotFound("Товары пропали или их пока нет");
+                return Ok(clients);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{goodsId}")]
+        public async Task<IActionResult> Get(Guid goodsId)
+        {
+            try
+            {
+                var client = await _repository.Get(goodsId);
+                if (client == null) return NotFound("Клиент не найден");
+                return Ok(client);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public async Task Delete(Guid id) => await _repository.Delete(id);
+
+        [HttpPost]
+        public async Task<IActionResult> Create(GoodsDto newGoods)
+        {
+            try
+            {
+                await _repository.Create(newGoods);
+                return Ok("Клиент создан");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
--- MRP_Admin_Api/Controllers/StorehouseController.cs
+++ MRP_Admin_Api/Controllers/StorehouseController.cs
@@ -1,12 +1,68 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExternalModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MRP_DAL.Repository;
+using MRP_DAL;
 
 namespace MRP_Admin_Api.Controllers
 {
+    [ApiController]
+
+    [Route("api/admin/storehouses")]
     public class StorehouseController : Controller
     {
-        public IActionResult Index()
+        private readonly StorehouseRepository _repository;
+
+        public StorehouseController(DbContextOptions<AppDbContext> db)
         {
-            return View();
+            _repository = new StorehouseRepository(db);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var clients = await _repository.GetAll();
+                if (clients == null) return NotFound("Склады пропали или их пока нет");
+                return Ok(clients);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{clientId}")]
+        public async Task<IActionResult> Get(Guid clientId)
+        {
+            try
+            {
+                var client = await _repository.Get(clientId);
+                if (client == null) return NotFound("Клиент не найден");
+                return Ok(client);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public async Task Delete(Guid id) => await _repository.Delete(id);
+
+        [HttpPost]
+        public async Task<IActionResult> Create(StorehouseDto newStorehouse)
+        {
+            try
+            {
+                await _repository.Create(newStorehouse);
+                return Ok("Клиент создан");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
